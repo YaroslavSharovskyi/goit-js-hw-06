fc4a40a0454249a8ec8d673957a14d091b2dcf6a
YaroslavSharovskyi <yaroslav.sharovskij@gmail.com>
2022-02-09 00:27:12 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,29 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+// const itemEl = document.createElement(`li`);
+// itemEl.textContent = ingredients;
+// itemEl.classList.add(`item`);
+
+
+// console.log(itemEl);
+
+
+const makeIngredients = ingredients => {
+  const itemIngredients = [];
+  for (const ingredient of ingredients) {
+  const itemEl = document.createElement(`li`);
+  itemEl.textContent = ingredient;
+  itemEl.classList.add(`item`);
+  return itemEl;
+  }
+
+  return itemIngredients.push(itemEl);
+}
+
+
+console.log(makeIngredients(ingredients));
+
+
