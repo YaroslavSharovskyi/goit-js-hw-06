33f25f45789e24c3d942685806f3449c076049c3
YaroslavSharovskyi <yaroslav.sharovskij@gmail.com>
2022-02-14 21:45:43 +0200

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,24 @@
+
+
+
+
+const btnDecrease = document.querySelector('[data-action = "decrement"]');
+const btnIncrease = document.querySelector('[data-action="increment"]');
+const value = document.querySelector('#value');
+
+btnDecrease.addEventListener('click', onBtnDecreaseClick);
+btnIncrease.addEventListener('click', onBtnIncreaseClick);
+
+
+let counterValue = 0;
+
+function onBtnDecreaseClick () {
+    counterValue--;
+   value.textContent = counterValue;
+}
+
+function onBtnIncreaseClick () {
+    counterValue++;
+    value.textContent = counterValue;
+}
+
