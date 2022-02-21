60fe6950c65e17a235da16df3fe719989a9168f7
YaroslavSharovskyi <yaroslav.sharovskij@gmail.com>
2022-02-21 23:31:17 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnEl = document.querySelector('button.change-color');
+const spanEl = document.querySelector('span.color');
+btnEl.addEventListener('click', onButtonClickChangeColor);
+
+
+
+function onButtonClickChangeColor () {
+const color = getRandomHexColor();
+document.body.setAttribute('style', `background-color: ${color}`);
+spanEl.textContent = color;
+};
