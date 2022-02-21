d3e17600478a095f1586d590c2cb1fe91a21947b
YaroslavSharovskyi <yaroslav.sharovskij@gmail.com>
2022-02-21 23:23:17 +0200

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,29 @@
+const formEl = document.querySelector("form.login-form");
+
+console.log(formEl);
+
+formEl.addEventListener('submit', onFormEvent);
+
+
+
+function onFormEvent (event) {
+    event.preventDefault();
+    
+const formElements = event.currentTarget.elements;
+const mail = formElements.email.value;
+const password = formElements.password.value;
+    
+if (mail === '' || password === '') {
+return alert(`Все поля формы должны быть заполнены!`);
+}
+    
+console.log({
+mail,
+password,
+});
+    
+    
+event.currentTarget.reset();
+};
+
 
